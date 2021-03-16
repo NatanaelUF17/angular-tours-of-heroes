6b607e403667bfc384e029496006c446d4a13067
Natanael <NatanaelUF17@gmail.com>
2021-03-15 23:36:07 -0400

    Initializing project

---
+++ frontend/src/app/mock/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from '../models/Hero';
+
+export const HEROES: Hero[] = [
+
+    { id: 11, name: 'Cristal' },
+    { id: 12, name: 'Spiderman' },
+    { id: 13, name: 'Batman' },
+    { id: 14, name: 'Superman' },
+    { id: 15, name: 'Ironman' },
+    { id: 16, name: 'Wolverine' },
+    { id: 17, name: 'Thor' },
+    { id: 18, name: 'Hulk' },
+    { id: 19, name: 'Capitan America' },
+    { id: 20, name: 'Pantera Negra' },
+];
